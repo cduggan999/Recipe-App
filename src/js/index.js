06775cc9bc440165760bede999ddff465789a5dc
Ciaran Duggan <cduggan999@gmail.com>
2020-01-04 00:17:36 +0000

    Added new function to serch controller

--- src/js/index.js
+++ src/js/index.js
@@ -19,7 +19,38 @@
 
 import Search from './models/Search';
 
-const search = new Search('pasta');
+/* Gloabal state of the applicationCache 
+ * - Search Object
+ * - Current recipe object
+ * - Shopping List object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1. Get query from view
+    const query = 'pizza' // todo
+
+    if (query) {
+        // 2. New serch object and add to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for results
+
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        // 5. Render results on UI
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+//const search = new Search('pasta');
 //console.log(search);
-search.getResults();
-// 140
+//search.getResults();
+// 142
