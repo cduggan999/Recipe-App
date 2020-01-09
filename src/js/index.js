42956c7655e7a0ac92a2433350e1196183d607d3
Ciaran Duggan <cduggan999@gmail.com>
2020-01-09 00:58:20 +0000

    Updated searchView to display recipe results on screen

--- src/js/index.js
+++ src/js/index.js
@@ -18,6 +18,8 @@
 // 139 09:00
 
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 /* Gloabal state of the applicationCache 
  * - Search Object
@@ -29,23 +31,26 @@ const state = {};
 
 const controlSearch = async () => {
     // 1. Get query from view
-    const query = 'pizza' // todo
+   const query = searchView.getInput();
 
     if (query) {
         // 2. New serch object and add to state
         state.search = new Search(query);
 
         // 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
 
         // 4. Search for recipes
         await state.search.getResults();
 
         // 5. Render results on UI
-        console.log(state.search.result);
+       // console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,3 +1,38 @@
-/* export const add = (a, b) => a + b;
-export const multiply = (a, b) => a * b;
-export const ID = 4; */
+import { elements } from './base';
+
+export const getInput = () => elements.searchInput.value;
+
+export const clearInput = () => {
+    elements.searchInput.value='';
+};
+
+export const clearResults = () => {
+    elements.searchResList.innerHTML='';
+}
+
+const renderRecipe = recipe => {
+    const size = '90x90';
+    const type = '.jpg';
+    console.log(recipe);
+    const markup = `
+        <li>
+            <a class="results__link results__link" href="#${recipe.id}">
+                <figure class="results__fig">
+                    <img src="https://spoonacular.com/recipeImages/${recipe.id}-${size}${type}" alt="${recipe.title}">
+                </figure>
+                <div class="results__data">
+                    <h4 class="results__name">${recipe.title} ...</h4>
+                    <p class="results__ready">Ready in ${recipe.readyInMinutes} minutes.</p>
+                    <p class="results__servings">Servings: ${recipe.servings}</p>
+                </div>
+            </a>
+        </li>
+    `; 
+    elements.searchResList.insertAdjacentHTML('beforeend', markup);
+};
+
+export const renderResults = recipes => {
+    // Loop through each item and print to screen
+    console.log(recipes);
+    recipes.forEach(renderRecipe);
+};
