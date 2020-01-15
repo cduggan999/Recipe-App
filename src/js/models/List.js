fbf7ccaf34c48be060743057d4b9e211a4ffcc3b
Ciaran Duggan <cduggan999@gmail.com>
2020-01-15 11:31:05 +0000

    Created List model

---
+++ src/js/models/List.js
@@ -0,0 +1,31 @@
+/* import axios from 'axios';
+import { key, url } from '../config'; */
+import uniqid from 'uniqid';
+
+export default class List {
+    constructor() {
+        this.items = [];
+    }
+
+    addItem (amount, unit, ingredient) {
+        const item = {
+            id: uniqid(),
+            amount,
+            unit,
+            ingredient
+        }
+        this.items.push(item);
+        return item;
+    }
+
+    deleteItem (id) {
+        const index = this.items.findIndex(el => el.id === id);
+        // Takes start index and number to cut from array
+        this.items.splice(index, 1);
+    }
+
+    updateAmount(id, newAmount) {
+        // Finds item with matching id and updates amount
+        this.items.find(el => el.id === id).amount = newAmount;
+    }
+}
